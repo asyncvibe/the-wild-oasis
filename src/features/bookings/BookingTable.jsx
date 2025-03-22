5e38fa6893bf2bb1d2edd7c644bea44cad9009a0
Waseemkhan950 <waseemadil950@gmail.com>
2025-03-22 23:42:47 +0500

    applied server side filtering and sorting on the bookings table

--- src/features/bookings/BookingTable.jsx
+++ src/features/bookings/BookingTable.jsx
@@ -9,7 +9,7 @@ function BookingTable() {
 
 	if (isPending) return <Spinner />;
 
-	if (!bookings.length) return <Empty resource="bookings" />;
+	if (!bookings?.length) return <Empty resource="bookings" />;
 	return (
 		<Menus>
 			<Table columns="0.6fr 2fr 2.4fr 1.4fr 1fr 3.2rem">
--- src/features/bookings/useReadBookings.js
+++ src/features/bookings/useReadBookings.js
@@ -1,14 +1,27 @@
 import { useQuery } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
 
 export function useReadBookings() {
+	const [searchParams] = useSearchParams();
+	// Filter
+	const filterValue = searchParams.get("status");
+	const filter =
+		!filterValue || filterValue === "all"
+			? null
+			: { field: "status", filterValue: filterValue };
+	// Sort
+	const sortOption = searchParams.get("sortBy") || "startDate-desc";
+	const [field, direction] = sortOption.split("-");
+	const sortBy = { field, direction };
 	const {
 		isPending,
 		data: bookings,
 		error,
 	} = useQuery({
-		queryKey: ["bookings"],
-		queryFn: getBookings,
+		queryKey: ["bookings", filter, sortBy],
+		queryFn: () => getBookings(filter, sortBy),
 	});
 	return { isPending, bookings, error };
 }
+// in queryKey array, we can pass multiple values; now this array depeneds on filter whenever the value of filter changes the react query will refetch the data.
