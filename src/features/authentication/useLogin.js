24c490e0b504e657102df6c0dc4a41382263dbde
Waseemkhan950 <waseemadil950@gmail.com>
2025-03-24 22:10:54 +0500

    fixed the small bug in the login functionality..

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,12 +1,14 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 import { login as loginApi } from "../../services/apiAuth";
 export function useLogin() {
 	const navigate = useNavigate();
+	const queryClient = useQueryClient();
 	const { mutate: login, isPending } = useMutation({
 		mutationFn: ({ email, password }) => loginApi({ email, password }),
-		onSuccess: () => {
+		onSuccess: (user) => {
+			queryClient.setQueryData(["user"], user.user);
 			navigate("/dashboard", { replace: true });
 		},
 		onError: (error) => {
